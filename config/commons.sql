c7a3c507d81f2de092324c41d1ba5a44327c9fb3
lysa <lysafichot@gmail.com>
2020-06-05 16:03:00 +0200

    database.sql

---
+++ config/commons.sql
@@ -0,0 +1,156 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.5deb2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:3306
+-- Généré le : ven. 05 juin 2020 à 15:57
+-- Version du serveur :  10.3.22-MariaDB-1ubuntu1
+-- Version de PHP : 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `commons`
+--
+CREATE DATABASE IF NOT EXISTS `commons` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `commons`;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `follow`
+--
+
+CREATE TABLE `follow` (
+  `id_followed` int(11) NOT NULL,
+  `id_follower` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `tweet`
+--
+
+CREATE TABLE `tweet` (
+  `id` int(11) NOT NULL,
+  `content` varchar(255) NOT NULL,
+  `id_user` int(11) DEFAULT NULL,
+  `created_at` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(100) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`) VALUES
+(37, '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user_like`
+--
+
+CREATE TABLE `user_like` (
+  `id_tweet` int(11) DEFAULT NULL,
+  `id_user` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `follow`
+--
+ALTER TABLE `follow`
+  ADD KEY `id_followed` (`id_followed`),
+  ADD KEY `id_follower` (`id_follower`);
+
+--
+-- Index pour la table `tweet`
+--
+ALTER TABLE `tweet`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Index pour la table `user_like`
+--
+ALTER TABLE `user_like`
+  ADD KEY `id_tweet` (`id_tweet`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `tweet`
+--
+ALTER TABLE `tweet`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `follow`
+--
+ALTER TABLE `follow`
+  ADD CONSTRAINT `follow_ibfk_1` FOREIGN KEY (`id_follower`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `follow_ibfk_2` FOREIGN KEY (`id_followed`) REFERENCES `user` (`id`);
+
+--
+-- Contraintes pour la table `tweet`
+--
+ALTER TABLE `tweet`
+  ADD CONSTRAINT `tweet_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`);
+
+--
+-- Contraintes pour la table `user_like`
+--
+ALTER TABLE `user_like`
+  ADD CONSTRAINT `user_like_ibfk_1` FOREIGN KEY (`id_tweet`) REFERENCES `tweet` (`id`),
+  ADD CONSTRAINT `user_like_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
